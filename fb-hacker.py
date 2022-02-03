a036bbd0202721b637433896b899811b33a4a03a
FananX9-2 <85710756+FananX9-2@users.noreply.github.com>
2022-02-03 13:30:20 +0100

    Add files via upload

---
+++ fb-hacker.py
@@ -0,0 +1,34 @@
+import os
+os.system ('pkg install cmatrix -y')
+os.system ('clear')
+os.system ('pkg install figlet -y')
+os.system ('clear')
+print ("\033[1;31m")
+os.system ('figlet Hk Feacbook')
+print ("")
+print ("")
+print ('  \033[1;32m{1}\033[1;33m hacker facebook')
+print ("")
+print ('  \033[1;32m{2}\033[1;33m update fx9')
+print ("")
+print ('\033[1;32mFor Example, Choose a Number \033[1;31m1,2,')
+v7x2 = input (" \033[1;33mhacker\033[1;31m==> ")
+if v7x2 == '1' :
+        os.system ('clear')
+        print ('')
+        print ("Loading..........!")
+        os.system ('figlet hk facebook')
+fx9 = input (" \033[1;33mname facebook ==> ")
+if fx9 == 'Maroine Astati' :
+        os.system ('python root2.py')
+if fx9 == 'Maroine Astati ' :
+        os.system ('python root2.py')
+if fx9 == 'maroine astati' :
+        os.system ('python root2.py')
+if fx9 == 'MaroineAstati' :
+        os.system ('python root2.py')
+if v7x2 == '2' :
+        os.system('xdg-open https://shortest.link/HLw')
+
+
+
